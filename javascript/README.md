4343a7801d7ea6055ce6e790576481b552fb684c
Tom Tilly <tomtillistrand@gmail.com>
2019-06-01 16:59:19 -0400

    add readme

---
+++ javascript/README.md
@@ -0,0 +1,5 @@
+# Compass and Speedometer
+
+## Note
+
+Site needs to be loaded over https on a mobile device in order to work.
--- javascript/javascript.js
+++ javascript/javascript.js
@@ -3,7 +3,6 @@ const speedEl = document.querySelector('.speed');
 
 navigator.geolocation.watchPosition((position) => {
   const { speed = 0, heading = 0 } = position.coords;
-  console.log(speed, heading);
   compass.style.transform = `rotate(${heading}deg)`;
   speedEl.textContent = speed;
 }, (err) => {
